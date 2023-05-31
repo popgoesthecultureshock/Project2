c353117e0886da8f2068ddf4b21a9c22e4bb1e5f
Cody Garcia <codygarcia0923@gmail.com>
2023-05-31 16:46:46 -0400

    dreams tests commit

--- controllers/dreams.js
+++ controllers/dreams.js
@@ -7,22 +7,21 @@ const index = async (req, res) => {
 }
 
 const newDream = (req, res) => {
-  res.render('dreams/new', { title: 'Add Dream' }).catch((err) => {
+  try {
+    res.render('dreams/new', { title: 'Add Dream' })
+  } catch (err) {
     res.redirect('/index')
     console.log(err, 'newDream Controller Not Working')
-  })
+  }
 }
 
 const createDream = async (req, res) => {
   try {
     const dream = await Dream.create(req.body)
-    console.log('THIS IS THE DREAM', dream)
     const person = req.user._id
-    const user = await User.find({ _id: person })
-    console.log('THIS IS THE USER', user)
-    user.dreams.push(dream)
+    const user = await User.findById(person)
+    user.dreams.push(dream._id)
     user.save()
-    // console.log('THIS IS THE USER', user)
     res.redirect(`/dreams/`)
   } catch (err) {
     console.log(err)
